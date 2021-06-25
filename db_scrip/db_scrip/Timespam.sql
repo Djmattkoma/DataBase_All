80db3d98df91033dabafafdf33437549860ba27d
Djmattkoma <55582553+Djmattkoma@users.noreply.github.com>
2021-06-25 18:14:47 +0200

    Initial commit

---
+++ db_scrip/db_scrip/Timespam.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Timespam]
+(
+	[code] Varchar(10) NOT NULL PRIMARY KEY, 
+    [start] TIMESTAMP NOT NULL, 
+    [end] TIMESTAMP NOT NULL, 
+    [pause_start] TIMESTAMP NULL, 
+    [pause_end] NCHAR(10) NULL
+)
---
+++ db_scrip/db_scrip/Timetable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Timetable]
+(
+	[name] INT NOT NULL PRIMARY KEY, 
+    [timespam] TIMESTAMP NOT NULL, 
+    [group] VARCHAR(20) NOT NULL, 
+    [categori] VARCHAR(20) NOT NULL, 
+    [seven] VARCHAR(10) NOT NULL
+)
